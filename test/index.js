2c6b154b6f0cb4869b5636bd459ed294572dca05
Nathan LaFreniere <quitlahok@gmail.com>
2015-03-21 18:10:58 -0700

    use promises, sync with wadofgum@3.x

--- test/index.js
+++ test/index.js
@@ -1,14 +1,15 @@
 var Joi = require('joi');
 var NeDB = require('nedb');
 var Plugin = require('..');
-var WOG = require('wadofgum');
+var Promise = require('bluebird');
+var Factory = require('wadofgum');
 
 var lab = exports.lab = require('lab').script();
 var expect = require('code').expect;
 
 lab.test('it requires an nedb database to load', function (done) {
 
-    var Broken = new WOG({
+    var Broken = new Factory({
         type: 'user',
         schema: {
             id: Joi.string(),
@@ -37,7 +38,7 @@ var db = new NeDB();
 
 lab.test('it can be loaded', function (done) {
 
-    User = new WOG({
+    User = new Factory({
         type: 'user',
         schema: {
             id: Joi.string(),
@@ -59,7 +60,7 @@ lab.test('it can be loaded', function (done) {
 lab.test('it fails to save when the model fails validation', function (done) {
 
     var user = new User();
-    user.save(function (err) {
+    user.save().catch(function (err) {
 
         expect(err).to.exist();
         done();
@@ -68,7 +69,7 @@ lab.test('it fails to save when the model fails validation', function (done) {
 
 lab.test('it can prevent saving by passing an error in preSave', function (done) {
 
-    var Broken = new WOG({
+    var Broken = new Factory({
         type: 'broken',
         schema: {
             id: Joi.string(),
@@ -83,24 +84,25 @@ lab.test('it can prevent saving by passing an error in preSave', function (done)
         }
     });
 
-    Broken.prototype.on('preSave', function (broken, next) {
+    Broken.prototype.on('preSave', function (broken) {
 
-        next(new Error('failed'));
+        return Promise.reject(new Error('failed'));
     });
 
     var broken = new Broken({ name: 'test' });
-    broken.save(function (err) {
+    broken.save()
+        .catch(function (err) {
 
-        expect(err).to.exist();
-        expect(err.message).to.equal('failed');
-        expect(broken.id).to.not.exist();
-        done();
-    });
+            expect(err).to.exist();
+            expect(err.message).to.equal('failed');
+            expect(broken.id).to.not.exist();
+            done();
+        });
 });
 
 lab.test('it can pass an error from postSave', function (done) {
 
-    var Broken = new WOG({
+    var Broken = new Factory({
         type: 'broken',
         schema: {
             id: Joi.string(),
@@ -115,19 +117,20 @@ lab.test('it can pass an error from postSave', function (done) {
         }
     });
 
-    Broken.prototype.on('postSave', function (broken, next) {
+    Broken.prototype.on('postSave', function (broken) {
 
-        next(new Error('failed'));
+        return Promise.reject(new Error('failed'));
     });
 
     var broken = new Broken({ name: 'test' });
-    broken.save(function (err) {
+    broken.save()
+        .catch(function (err) {
 
-        expect(err).to.exist();
-        expect(err.message).to.equal('failed');
-        expect(broken.id).to.exist();
-        done();
-    });
+            expect(err).to.exist();
+            expect(err.message).to.equal('failed');
+            expect(broken.id).to.exist();
+            done();
+        });
 });
 
 var userId;
@@ -136,90 +139,87 @@ lab.test('it can save a model', function (done) {
 
     var user = new User({ name: 'test' });
     expect(user.id).to.not.exist();
-    user.save(function (err) {
+    user.save()
+        .then(function () {
 
-        expect(err).to.not.exist();
-        expect(user.id).to.exist();
-        expect(user._type).to.not.exist();
-        expect(user._id).to.not.exist();
-        userId = user.id;
-        done();
-    });
+            expect(user.id).to.exist();
+            expect(user._type).to.not.exist();
+            expect(user._id).to.not.exist();
+            userId = user.id;
+            done();
+        });
 });
 
 lab.test('it can find all models', function (done) {
 
-    User.find(function (err, users) {
+    User.find()
+        .then(function (users) {
 
-        expect(err).to.not.exist();
-        expect(users).to.be.an.array();
-        expect(users).to.have.length(1);
-        done();
-    });
+            expect(users).to.be.an.array();
+            expect(users).to.have.length(1);
+            done();
+        });
 });
 
 lab.test('it can find models with a query', function (done) {
 
-    User.find({ name: 'test' }, function (err, users) {
+    User.find({ name: 'test' })
+        .then(function (users) {
 
-        expect(err).to.not.exist();
-        expect(users).to.be.an.array();
-        expect(users).to.have.length(1);
-        done();
-    });
+            expect(users).to.be.an.array();
+            expect(users).to.have.length(1);
+            done();
+        });
 });
 
 lab.test('it can count models', function (done) {
 
-    User.count(function (err, users) {
+    User.count()
+        .then(function (users) {
 
-        expect(err).to.not.exist();
-        expect(users).to.equal(1);
+            expect(users).to.equal(1);
+            return User.count({ name: 'banana' });
+        })
+        .then(function (users) {
 
-        User.count({ name: 'banana' }, function (err, users) {
-
-            expect(err).to.not.exist();
             expect(users).to.equal(0);
             done();
         });
-    });
 });
 
 lab.test('it can fetch a model by id', function (done) {
 
-    User.get(userId, function (err, user) {
+    User.get(userId)
+        .then(function (user) {
 
-        expect(err).to.not.exist();
-        expect(user.id).to.equal(userId);
-        done();
-    });
+            expect(user.id).to.equal(userId);
+            done();
+        });
 });
 
 lab.test('returns a not found error when fetching a model by id that doesnt exist', function (done) {
 
-    User.get('bananas', function (err, user) {
+    User.get('bananas')
+        .catch(function (err) {
 
-        expect(err).to.exist();
-        expect(err.message).to.equal('not found');
-        done();
-    });
+            expect(err).to.exist();
+            expect(err.message).to.equal('not found');
+            done();
+        });
 });
 
 lab.test('can update a document', function (done) {
 
-    User.get(userId, function (err, user) {
+    User.get(userId).then(function (user) {
 
-        expect(err).to.not.exist();
         expect(user.id).to.equal(userId);
-        user.update({ $set: { name: 'testing' } }, function (err, updated) {
+        user.update({ $set: { name: 'testing' } }).then(function (user) {
 
-            expect(err).to.not.exist();
-            expect(updated.name).to.equal('testing');
-
-            updated.update({ $set: { _id: 'bananas' } }, {}, function (err, updated) {
+            expect(user.name).to.equal('testing');
+            user.update({ $set: { _id: 'bananas' } }, {}).catch(function (err) {
 
                 expect(err).to.exist();
-                expect(err).to.equal('You can\'t change a document\'s _id');
+                expect(err.message).to.equal('You can\'t change a document\'s _id');
                 done();
             });
         });
@@ -228,34 +228,39 @@ lab.test('can update a document', function (done) {
 
 lab.test('can batch update documents', function (done) {
 
-    User.update({ name: 'testing' }, { $set: { name: 'test' } }, function (err, num) {
+    User.update({ name: 'testing' }, { $set: { name: 'test' } }, {})
+        .then(function (num) {
 
-        expect(err).to.not.exist();
-        expect(num).to.equal(1);
-        User.update({ name: 'test' }, { $set: { name: 'testing' } }, {}, function (err, num) {
+            expect(num).to.equal(1);
+            return User.update({ name: 'test' }, { $set: { name: 'testing' } });
+        })
+        .then(function (num) {
 
-            expect(err).to.not.exist();
+            expect(num).to.equal(1);
+            return User.update({ $set: { name: 'test' } });
+        })
+        .then(function (num) {
+
             expect(num).to.equal(1);
             done();
         });
-    });
 });
 
 lab.test('can remove a document', function (done) {
 
-    User.get(userId, function (err, user) {
+    User.get(userId)
+        .then(function (user) {
 
-        expect(err).to.not.exist();
-        expect(user.id).to.equal(userId);
-        user.remove(function (err) {
+            expect(user.id).to.equal(userId);
+            return user.remove();
+        })
+        .then(function () {
+            return User.get(userId);
+        })
+        .catch(function (err) {
 
-            expect(err).to.not.exist();
-            User.get(userId, function (err) {
-
-                expect(err).to.exist();
-                expect(err.message).to.equal('not found');
-                done();
-            });
+            expect(err).to.exist();
+            expect(err.message).to.equal('not found');
+            done();
         });
-    });
 });
