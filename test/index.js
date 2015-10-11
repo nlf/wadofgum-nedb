580f4f8a4dda218af1f57242eb75db1105a5ce83
Nathan LaFreniere <quitlahok@gmail.com>
2015-10-10 18:06:08 -0700

    4.0.0

--- test/index.js
+++ test/index.js
@@ -1,266 +1,355 @@
-var Joi = require('joi');
-var NeDB = require('nedb');
-var Plugin = require('..');
-var Promise = require('bluebird');
-var Factory = require('wadofgum');
+'use strict';
 
-var lab = exports.lab = require('lab').script();
-var expect = require('code').expect;
+const Joi = require('joi');
+const NeDB = require('nedb');
+const Mixin = require('..');
+const Validation = require('wadofgum-validation');
+const Wadofgum = require('wadofgum');
 
-lab.test('it requires an nedb database to load', function (done) {
-
-    var Broken = new Factory({
-        type: 'user',
-        schema: {
-            id: Joi.string(),
-            name: Joi.string()
-        }
-    });
-
-    expect(function () {
-
-        Broken.register(Plugin);
-    }).to.throw();
-
-    expect(function () {
-
-        Broken.register({
-            register: Plugin,
-            options: {}
-        });
-    }).to.throw();
-
-    done();
-});
-
-var User;
-var db = new NeDB();
+let lab = exports.lab = require('lab').script();
+let expect = require('code').expect;
 
 lab.test('it can be loaded', function (done) {
 
-    User = new Factory({
-        type: 'user',
-        schema: {
-            id: Joi.string(),
-            name: Joi.string().required()
-        }
-    });
-
-    expect(function () {
-
-        User.register({
-            register: Plugin,
-            options: { db: db }
-        });
-    }).to.not.throw();
-
+    class User extends Wadofgum.mixin(Mixin) {};
+    expect(User).to.exist();
+    expect(User.capabilities.has('nedb')).to.equal(true);
     done();
 });
 
-lab.test('it fails to save when the model fails validation', function (done) {
+lab.test('it can have a db associated', function (done) {
 
-    var user = new User();
-    user.save().catch(function (err) {
-
-        expect(err).to.exist();
-        done();
-    });
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.db = new NeDB();
+    expect(User.meta.get('db')).to.contain('insert', 'find', 'count', 'update', 'remove', 'raw');
+    done();
 });
 
-lab.test('it can prevent saving by passing an error in preSave', function (done) {
+lab.test('it does nothing when attempting to set a schema without a validation mixin', function (done) {
 
-    var Broken = new Factory({
-        type: 'broken',
-        schema: {
-            id: Joi.string(),
-            name: Joi.string().required()
-        }
-    });
-
-    Broken.register({
-        register: Plugin,
-        options: {
-            db: db
-        }
-    });
-
-    Broken.prototype.on('preSave', function (broken) {
-
-        return Promise.reject(new Error('failed'));
-    });
-
-    var broken = new Broken({ name: 'test' });
-    broken.save()
-        .catch(function (err) {
-
-            expect(err).to.exist();
-            expect(err.message).to.equal('failed');
-            expect(broken.id).to.not.exist();
-            done();
-        });
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.schema = { name: Joi.string() };
+    expect(User.meta.has('keys')).to.equal(false);
+    done();
 });
-
-lab.test('it can pass an error from postSave', function (done) {
-
-    var Broken = new Factory({
-        type: 'broken',
-        schema: {
-            id: Joi.string(),
-            name: Joi.string().required()
-        }
-    });
-
-    Broken.register({
-        register: Plugin,
-        options: {
-            db: db
-        }
-    });
-
-    Broken.prototype.on('postSave', function (broken) {
-
-        return Promise.reject(new Error('failed'));
-    });
-
-    var broken = new Broken({ name: 'test' });
-    broken.save()
-        .catch(function (err) {
-
-            expect(err).to.exist();
-            expect(err.message).to.equal('failed');
-            expect(broken.id).to.exist();
-            done();
-        });
-});
-
-var userId;
 
 lab.test('it can save a model', function (done) {
 
-    var user = new User({ name: 'test' });
-    expect(user.id).to.not.exist();
-    user.save()
-        .then(function () {
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.db = new NeDB();
+    let user = new User({ name: 'test' });
+    user.save().then(() => {
 
-            expect(user.id).to.exist();
-            expect(user._type).to.not.exist();
-            expect(user._id).to.not.exist();
-            userId = user.id;
-            done();
-        });
+        expect(user).to.contain('name', '_type', '_id');
+        expect(user._type).to.equal(user.constructor.name);
+        done();
+    }).catch(done);
 });
 
-lab.test('it can find all models', function (done) {
+lab.test('it rejects when attempting to save when no db is assigned', function (done) {
 
-    User.find()
-        .then(function (users) {
+    class User extends Wadofgum.mixin(Mixin) {};
+    let user = new User({ name: 'test' });
+    user.save().catch((err) => {
 
-            expect(users).to.be.an.array();
-            expect(users).to.have.length(1);
-            done();
-        });
+        expect(err.message).to.equal('No db assigned to model class: User');
+        done();
+    }).catch(done);
 });
 
-lab.test('it can find models with a query', function (done) {
+lab.test('it can fetch a model given its id', function (done) {
 
-    User.find({ name: 'test' })
-        .then(function (users) {
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.db = new NeDB();
+    let user = new User({ name: 'test' });
+    user.save().then(() => {
 
-            expect(users).to.be.an.array();
-            expect(users).to.have.length(1);
+        let user2 = new User({ _id: user._id });
+        expect(user2.name).to.not.exist();
+        return user2.get().then(() => {
+
+            expect(user2.name).to.equal(user.name);
             done();
         });
+    }).catch(done);
+});
+
+lab.test('it returns an error when an id is not found', function (done) {
+
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.db = new NeDB();
+    let user = new User({ name: 'test' });
+    user.get().catch((err) => {
+
+        expect(err.message).to.equal('Not found');
+        done();
+    }).catch(done);
+});
+
+lab.test('it can remove a model', function (done) {
+
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.db = new NeDB();
+    let user = new User({ name: 'test' });
+    user.save().then(() => {
+
+        let id = user._id;
+        return user.remove().then(() => {
+
+            return User.findOne({ _id: id });
+        }).catch((err) => {
+
+            expect(user.name).to.not.exist();
+            expect(err.message).to.equal('Not found');
+            done();
+        });
+    }).catch(done);
 });
 
 lab.test('it can count models', function (done) {
 
-    User.count()
-        .then(function (users) {
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.db = new NeDB();
+    User.count({ name: 'test' }).then((count) => {
 
-            expect(users).to.equal(1);
-            return User.count({ name: 'banana' });
-        })
-        .then(function (users) {
+        expect(count).to.equal(0);
+        let user = new User({ name: 'test' });
+        return user.save();
+    }).then(() => {
 
-            expect(users).to.equal(0);
-            done();
-        });
+        return User.count({ name: 'test' });
+    }).then((count) => {
+
+        expect(count).to.equal(1);
+        done();
+    }).catch(done);
 });
 
-lab.test('it can fetch a model by id', function (done) {
+lab.test('it can find models', function (done) {
 
-    User.get(userId)
-        .then(function (user) {
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.db = new NeDB();
+    let user = new User({ name: 'test' });
+    User.find({ name: 'test' }).then((models) => {
 
-            expect(user.id).to.equal(userId);
-            done();
-        });
+        expect(models.length).to.equal(0);
+        return user.save();
+    }).then(() => {
+
+        return User.find({ name: 'test' });
+    }).then((models) => {
+
+        expect(models.length).to.equal(1);
+        expect(models[0]).to.deep.equal(user);
+        done();
+    }).catch(done);
 });
 
-lab.test('returns a not found error when fetching a model by id that doesnt exist', function (done) {
+lab.test('it can find a single model', function (done) {
 
-    User.get('bananas')
-        .catch(function (err) {
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.db = new NeDB();
+    let user = new User({ name: 'test' });
+    User.findOne({ name: 'test' }).catch((err) => {
 
-            expect(err).to.exist();
-            expect(err.message).to.equal('not found');
-            done();
-        });
+        expect(err.message).to.equal('Not found');
+        return user.save();
+    }).then(() => {
+
+        return User.findOne({ name: 'test' });
+    }).then((model) => {
+
+        expect(model).to.deep.equal(user);
+        done();
+    }).catch(done);
 });
 
-lab.test('can update a document', function (done) {
+lab.test('it can update models with a full document', function (done) {
 
-    User.get(userId).then(function (user) {
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.db = new NeDB();
+    let user = new User({ name: 'test' });
+    User.find({ name: 'test' }).then((models) => {
 
-        expect(user.id).to.equal(userId);
-        user.update({ $set: { name: 'testing' } }).then(function (user) {
+        expect(models.length).to.equal(0);
+        return User.find({ name: 'not test' });
+    }).then((models) => {
 
-            expect(user.name).to.equal('testing');
-            user.update({ $set: { _id: 'bananas' } }, {}).catch(function (err) {
+        expect(models.length).to.equal(0);
+        return user.save();
+    }).then(() => {
 
-                expect(err).to.exist();
-                expect(err.message).to.equal('You can\'t change a document\'s _id');
+        return User.update({ name: 'test' }, { name: 'not test' });
+    }).then((count) => {
+
+        expect(count).to.equal(1);
+        return User.find({ name: 'not test' });
+    }).then((models) => {
+
+        expect(models.length).to.equal(1);
+        expect(models[0].name).to.equal('not test');
+        expect(models[0]._id).to.equal(user._id);
+        done();
+    }).catch(done);
+});
+
+lab.test('it can update models with a partial update', function (done) {
+
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.db = new NeDB();
+    let user = new User({ name: 'test' });
+    User.find({ name: 'test' }).then((models) => {
+
+        expect(models.length).to.equal(0);
+        return User.find({ name: 'not test' });
+    }).then((models) => {
+
+        expect(models.length).to.equal(0);
+        return user.save();
+    }).then(() => {
+
+        return User.update({ name: 'test' }, { $set: { name: 'not test' } });
+    }).then((count) => {
+
+        expect(count).to.equal(1);
+        return User.find({ name: 'not test' });
+    }).then((models) => {
+
+        expect(models.length).to.equal(1);
+        expect(models[0].name).to.equal('not test');
+        expect(models[0]._id).to.equal(user._id);
+        done();
+    }).catch(done);
+});
+
+lab.test('it can update models with options', function (done) {
+
+    class User extends Wadofgum.mixin(Mixin) {};
+    User.db = new NeDB();
+    let user = new User({ name: 'test' });
+    User.find({ name: 'test' }).then((models) => {
+
+        expect(models.length).to.equal(0);
+        return User.find({ name: 'not test' });
+    }).then((models) => {
+
+        expect(models.length).to.equal(0);
+        return user.save();
+    }).then(() => {
+
+        return User.update({ name: 'test' }, { name: 'not test' }, { multi: true });
+    }).then((count) => {
+
+        expect(count).to.equal(1);
+        return User.find({ name: 'not test' });
+    }).then((models) => {
+
+        expect(models.length).to.equal(1);
+        expect(models[0].name).to.equal('not test');
+        expect(models[0]._id).to.equal(user._id);
+        done();
+    }).catch(done);
+});
+
+lab.describe('when used with validation', function () {
+
+    lab.test('it can extend a given schema', function (done) {
+
+        class User extends Wadofgum.mixin(Validation, Mixin) {};
+        User.schema = Joi.object({ name: Joi.string() });
+        expect(User.meta.get('keys')).to.contain('name', '_id', '_type');
+        done();
+    });
+
+    lab.test('it can extend a given schema by plain object', function (done) {
+
+        class User extends Wadofgum.mixin(Validation, Mixin) {};
+        User.schema = { name: Joi.string() };
+        expect(User.meta.get('keys')).to.contain('name', '_id', '_type');
+        done();
+    });
+
+    lab.test('it validates before saving', function (done) {
+
+        class User extends Wadofgum.mixin(Validation, Mixin) {};
+        User.db = new NeDB();
+        User.schema = Joi.object({ name: Joi.string().required() });
+        let user = new User();
+        user.save().catch((err) => {
+
+            expect(err.name).to.equal('ValidationError');
+            done();
+        }).catch(done);
+    });
+
+    lab.test('it validates after saving', function (done) {
+
+        class User extends Wadofgum.mixin(Validation, Mixin) {};
+        User.db = new NeDB();
+        User.schema = Joi.object({ name: Joi.string().required() });
+        let user = new User({ name: 'test' });
+        user.save().then(() => {
+
+            expect(user).to.contain('_id', 'name');
+            expect(user).to.not.contain('_type');
+            done();
+        }).catch(done);
+    });
+
+    lab.test('it validates after getting a model instance', function (done) {
+
+        class User extends Wadofgum.mixin(Validation, Mixin) {};
+        User.db = new NeDB();
+        User.schema = Joi.object({ name: Joi.string().required() });
+        let user = new User({ name: 'test' });
+        user.save().then(() => {
+
+            expect(user).to.contain('_id', 'name');
+            expect(user).to.not.contain('_type');
+            let user2 = new User({ _id: user._id });
+            return user2.get().then(() => {
+
+                expect(user2).to.deep.equal(user);
                 done();
             });
-        });
+        }).catch(done);
+    });
+
+    lab.test('it validates after finding models', function (done) {
+
+        class User extends Wadofgum.mixin(Validation, Mixin) {};
+        User.db = new NeDB();
+        User.schema = Joi.object({ name: Joi.string().required() });
+        let user = new User({ name: 'test' });
+        user.save().then(() => {
+
+            expect(user).to.contain('_id', 'name');
+            expect(user).to.not.contain('_type');
+            return User.find({ name: 'test' });
+        }).then((users) => {
+
+            expect(users.length).to.equal(1);
+            expect(users[0]).to.deep.equal(user);
+            done();
+        }).catch(done);
+    });
+
+    lab.test('it validates after finding one model', function (done) {
+
+        class User extends Wadofgum.mixin(Validation, Mixin) {};
+        User.db = new NeDB();
+        User.schema = Joi.object({ name: Joi.string().required() });
+        let user = new User({ name: 'test' });
+        user.save().then(() => {
+
+            expect(user).to.contain('_id', 'name');
+            expect(user).to.not.contain('_type');
+            return User.findOne({ name: 'test' });
+        }).then((innerUser) => {
+
+            expect(innerUser).to.deep.equal(user);
+            done();
+        }).catch(done);
     });
 });
 
-lab.test('can batch update documents', function (done) {
-
-    User.update({ name: 'testing' }, { $set: { name: 'test' } }, {})
-        .then(function (num) {
-
-            expect(num).to.equal(1);
-            return User.update({ name: 'test' }, { $set: { name: 'testing' } });
-        })
-        .then(function (num) {
-
-            expect(num).to.equal(1);
-            return User.update({ $set: { name: 'test' } });
-        })
-        .then(function (num) {
-
-            expect(num).to.equal(1);
-            done();
-        });
-});
-
-lab.test('can remove a document', function (done) {
-
-    User.get(userId)
-        .then(function (user) {
-
-            expect(user.id).to.equal(userId);
-            return user.remove();
-        })
-        .then(function () {
-            return User.get(userId);
-        })
-        .catch(function (err) {
-
-            expect(err).to.exist();
-            expect(err.message).to.equal('not found');
-            done();
-        });
-});
